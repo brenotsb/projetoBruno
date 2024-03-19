b5e49575fd2e775d96edfefed42fea02b466dd81
breni <breni@host.docker.internal>
2024-03-19 11:24:03 -0300

    novas alterações

--- MemoryManagerMax/src/so/SystemOperation.java
+++ MemoryManagerMax/src/so/SystemOperation.java
@@ -2,49 +2,29 @@ package so;
 
 import so.cpu.CpuManager;
 import so.memory.MemoryManager;
+import so.memory.Strategy;
 import so.schedule.Schedule;
 
 public class SystemOperation {
-	private static MemoryManager mm;
-	private static CpuManager cm;
-	private static Schedule schedule;
-	
-	public static MemoryManager getMm() {
-		return mm;
-	}
-	public static void setMm(MemoryManager mm) {
-		SystemOperation.mm = mm;
-	}
-	public static CpuManager getCm() {
-		return cm;
-	}
-	public static void setCm(CpuManager cm) {
-		SystemOperation.cm = cm;
-	}
-	public static Schedule getSchedule() {
-		return schedule;
-	}
-	public static void setSchedule(Schedule schedule) {
-		SystemOperation.schedule = schedule;
-	}
-	
-	
-	
-	public static Object systemCall(SystemCallType type, Process p) {
-		 if(type.equals(SystemCallType.WRITE_PROCESS)) {
-	
-		 } else if (type.equals(SystemCallType.DELETE_PROCESS)) {
-	
-		 } else if (type.equals(SystemCallType.CREATE_PROCESS)) {
-			 if(cm == null) {
-				 cm = new CpuManager();
-			 }
+	 private static MemoryManager mm;
+	 private static CpuManager cm;
+
+	public static Process SystemCall(SystemCallType type, Process p) {
+		if(type.equals(SystemCallType.WRITE_PROCESS)) {
+			mm.write(p);
+		} else if (type.equals(SystemCallType.READ_PROCESS)) {
+			
+		} else if (type.equals(SystemCallType.DELETE_PROCESS)) {
+			mm.delete(p);
+		} else if (type.equals(SystemCallType.CREATE_PROCESS)) {
 			 if (mm == null) {
 				 mm = new MemoryManager(Strategy.FIRST_FIT);
 			 }
-			 return systemCall(SystemCallType.CREATE_PROCESS, null);
-			 
-		 
-		 }
-	 }
+			 if (cm == null) {
+				 cm = new CpuManager();
+			 }
+			 return new Process();
+		} 	
+			return null;
+	}
 }
--- MemoryManagerMax/src/so/memory/MemoryManager.java
+++ MemoryManagerMax/src/so/memory/MemoryManager.java
@@ -1,9 +1,87 @@
 package so.memory;
 
+import so.Process;
+
 public class MemoryManager {
-	private Strategy strategy; 
+	
+	private String[] physicMemory;
+	private Strategy strategy;
 	
 	public MemoryManager(Strategy strategy) {
-		this.strategy = strategy
+		this.strategy = strategy;
+		physicMemory = new String[128];
+		
 	}
+		public void write (Process p){	
+			if(this.strategy.equals(Strategy.FIRST_FIT)) {
+				this.writeUsingFirstFit(p);
+			} else if (this.strategy.equals(Strategy.BEST_FIT)) {
+				this.writeUsingBestFit(p);
+			} else if (this.strategy.equals(Strategy.WORST_FIT)) {
+				this.writeUsingWorstFit(p);
+			} else if (this.strategy.equals(Strategy.PAGING)) {
+				this.writeUsingPaging(p);
+			}
+		}
+		private void writeUsingPaging(Process p) {
+			// TODO Auto-generated method stub
+			
+		}
+		private void writeUsingWorstFit(Process p) {
+			// TODO Auto-generated method stub
+			
+		}
+		private void writeUsingBestFit(Process p) {
+			// TODO Auto-generated method stub
+			
+		}
+		private void writeUsingFirstFit(Process p) {
+			int actualSize = 0;
+			for(int i = 0; i < physicMemory.length; i++ ) {
+				if (i == physicMemory.length - 1) {
+					if(actualSize > 0) {
+						if (p.getSizeInMemory() <= actualSize);
+						int start = i - actualSize;
+						int end = i;
+						AddressMemory address = new AddressMemory(start, end);
+						if(p.getSizeInMemory() <= address.getSize()) {
+							insertProcessInMemory(p, address);
+							break; 
+						}
+					}
+				
+				}else if(physicMemory[i] == null) {
+					actualSize++;
+				} else {
+					if(actualSize > 0) {
+						int start = i - actualSize;
+						int end = i - 1;
+						AddressMemory address = new AddressMemory(start, end);
+						if(p.getSizeInMemory() <= address.getSize()) {
+							insertProcessInMemory(p, address);
+							break; 
+						}
+					}
+					actualSize = 0;
+				}
+			}
+			printMemoryStatus();
+			
+		}
+		private void printMemoryStatus() {
+			for(int i = 0; i < physicMemory.length; i++) {
+				System.out.print(physicMemory[i] + " / ");
+			}
+			
+		}
+		private void insertProcessInMemory(Process p, AddressMemory address) {
+			for(int i = address.getStart(); i<= address.getEnd(); i++);
+			int i = 0;
+			this.physicMemory[i] = p.getId();
+			
+			
+		}
+		public void delete (Process p){	
+			
+		}
 }
